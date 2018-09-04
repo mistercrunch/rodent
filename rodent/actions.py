563d9c87c056ad70657eed0b9882290a256e4eb5
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-09-03 20:37:56 -0700

    Getting to a working state

--- rodent/actions.py
+++ rodent/actions.py
@@ -1,35 +1,60 @@
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+"""
 import os
+
+import click
 import git
 
 from rodent.licenses import LICENSE_MAP
 
-EXT_MAP = {
-    'js': 'javascript',
-    'jsx': 'javascript',
-    'py': 'python',
-}
+
+def js_wrapper(s):
+    lines = []
+    lines.append("/**")
+    lines += [" * " + line if line else " *" for line in s.split("\n")]
+    lines.append(" */")
+    return "\n".join(lines)
+
+
+EXT_MAP = {"js": "javascript", "jsx": "javascript", "py": "python"}
 LICENSE_WRAPPER = {
-    'python': lambda s: '"""\n{}\n"""'.format(s),
-    'javascript': lambda s: '/*\n{}\n*/'.format(s),
+    "python": lambda s: '"""\n{}\n"""'.format(s),
+    "javascript": js_wrapper,
 }
+SPECIAL_FIRST_LINES = ["#!", "# -*-"]
 
 
-def get_commented_license(license, language):
-    license_text = LICENSE_MAP[license]
-    license_mutator = LICENSE_WRAPPER[language]
-    return license_mutator(license_text)
+def get_commented_license(license_type, language):
+    license_text = LICENSE_MAP[license_type]
+    license_type_mutator = LICENSE_WRAPPER[language]
+    return license_type_mutator(license_text)
 
 
 def get_extension(filename):
-    splitted = filename.split('.')
-    return splitted[-1] if len(splitted) > 1 else ''
+    splitted = filename.split(".")
+    return splitted[-1] if len(splitted) > 1 else ""
 
 
-def files_in_scope(folder='./', extentions=None):
+def files_in_scope(folder="./", extentions=None):
     file_list = []
     if not extentions:
         extentions = EXT_MAP.keys()
-
 
     repo_files = git.cmd.Git().ls_files().split()
     for filename in repo_files:
@@ -38,24 +63,72 @@ def files_in_scope(folder='./', extentions=None):
             yield filename
 
 
-def list_files(folder='./', extentions=None):
-    for s in files_in_scope(folder, extentions):
-        print(s)
+def list_files(folder="./", extentions=None):
+    for filename in files_in_scope(folder, extentions):
+        print(filename)
 
 
-def apply(filepath, license_text):
-    file_content = read_file(filepath)
-    if not check_license(file_content, license_text):
-        print('APPLY TO: ' + filepath)
+def apply(license_type="asf", folder="./", extentions=None):
+    for filename in files_in_scope(folder, extentions):
+        apply_to_file(filename, license_type)
+    click.echo(click.style("All done! 🍰", fg="white"))
+
+
+def check(license_type="asf", folder="./", extentions=None):
+    failed = []
+    for filepath in files_in_scope(folder, extentions):
+        file_content = read_file(filepath)
+        language = get_language(filepath)
+        commented_license_text = get_commented_license(license_type, language)
+        if not check_license(file_content, language, license_type):
+            failed.append(filepath)
+    if failed:
+        click.echo(click.style("Check failed", fg="red"))
+        for s in failed:
+            click.echo(click.style("* " + s, fg="red"))
     else:
-        print('SKIPPING: ' + filepath)
+        click.echo(click.style("Check passed!", fg="green"))
 
 
-def check_license(file_content, license_text):
-    commented_license_text = get_commented_license(license, language)
+def get_language(filename):
+    ext = get_extension(filename)
+    return EXT_MAP.get(ext)
+
+
+def squeeze_in_license(file_content, commented_license_text):
+    """Skips shebang and adds license content"""
+    license_applied = False
+    newfile_lines = []
+    for line in file_content.split("\n"):
+        if not license_applied:
+            is_special_line = any(line.startswith(s) for s in SPECIAL_FIRST_LINES)
+            if not is_special_line:
+                newfile_lines.append(commented_license_text)
+                license_applied = True
+        newfile_lines.append(line)
+    return "\n".join(newfile_lines)
+
+
+def apply_to_file(filepath, license_type="asf"):
+    file_content = read_file(filepath)
+    language = get_language(filepath)
+    commented_license_text = get_commented_license(license_type, language)
+    if file_content and not check_license(file_content, language, license_type):
+        click.echo(click.style("[adding license] - " + filepath, fg="green"))
+        with open(filepath, "w") as f:
+            new_content = squeeze_in_license(file_content, commented_license_text)
+            f.write(new_content)
+    else:
+        click.echo(click.style("[skipping] - " + filepath, fg="red"))
+
+
+def check_license(file_content, language, license_type="asf"):
+    commented_license_text = get_commented_license(license_type, language)
+    if not file_content:
+        return True
     return commented_license_text in file_content
 
 
 def read_file(filepath):
-    with open(os.path.join(dir_name, filename), 'r') as f:
-        file_content = f.read()
+    with open(filepath, "r") as f:
+        return f.read()
--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -13,38 +31,33 @@ from setuptools import find_packages, setup
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-with io.open('README.md', encoding='utf-8') as f:
+with io.open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
-version_string = '0.1.0'
+version_string = "0.1.0"
 
 setup(
-    name='rodent',
+    name="rodent",
     description="A utility CLI to manage license headers",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     version=version_string,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    scripts=['rodent/bin/rodent'],
-    install_requires=[
-        'gitpython',
-        'click',
-        'colorama',
-    ],
-    author='Maxime Beauchemin',
-    author_email='maximebeauchemin@gmail.com',
-    url='https://github.com/mistercrunch/rodent',
+    scripts=["rodent/bin/rodent"],
+    install_requires=["gitpython", "click"],
+    author="Maxime Beauchemin",
+    author_email="maximebeauchemin@gmail.com",
+    url="https://github.com/mistercrunch/rodent",
     download_url=(
-        'https://github.com'
-        '/mistercrunch/rodent/tarball/' + version_string
+        "https://github.com" "/mistercrunch/rodent/tarball/" + version_string
     ),
     classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'License :: OSI Approved :: Apache Software License',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Apache Software License",
     ],
-    license='Apache License 2.0',
+    license="Apache License 2.0",
 )
